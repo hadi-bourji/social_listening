f3e136e3813134b41adacf0e3c4f2f0538f21bbe
hadibourji1 <hadibourji1+10@gmail.com>
2025-08-06 15:15:10 -0500

    added in function pick_relevant_articles which takes the list of article dictionaries and desired keywords and outputs a dict with article titles, links, and dates for articles that contain at least one of the keywords provided

--- script.py
+++ script.py
@@ -36,22 +36,26 @@ def extract_articles_from_rss(website: str):
     
 
 '''
-function takes "articles" (list of dictionaries where each dictionary contains the relevant information for one article)
+function takes "articles" (list of dictionaries where each dictionary contains the relevant information for one article) and keywords (list of keywords we want to find articles about) as input
+outputs relevant_articles list of articles that contain 1+ keywords
 '''
-def pick(articles):    
-    for key,value in articles[0].items():
-        print(f"{key}: {value}")
-pick(extract_articles_from_rss('https://feeds.nbcnews.com/nbcnews/public/news'))
+def pick_relevant_articles(articles: list, keywords: list):   
+    relevant_articles = {}
+
+    for article in articles:
+        for key, value in article.items(): #for each key value pair for each article, we confirm that the value is a string and then check if any of the keywords are in the value
+            if isinstance(value, str) and any(keyword.lower() in value.lower() for keyword in keywords):#if it is we take the article title, link, and publish time and put it in the dict relevant_articles which is the output
+                title = article.get('title')
+                link =article.get('link')
+                published = article.get('published')
+                relevant_articles[title] = {'link': link, 'published': published}
+                break
+    return relevant_articles
+pick_relevant_articles(extract_articles_from_rss('https://feeds.nbcnews.com/nbcnews/public/news'), ["Trump","fort"])
 
 
 
-
-
-
-
-
-
-#Use this code to see the all the articles
+# # Use this code to see the all the articles
 # website = 'https://feeds.nbcnews.com/nbcnews/public/news'
 # def view_all(website: str):
 #     article_dictionary = feedparser.parse(website) #load in rss news feed site and put all information in dict
