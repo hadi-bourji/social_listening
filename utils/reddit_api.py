af29fd3f9cbf26655127353d21a6c4adf9ffe510
hadibourji1 <hadibourji1@gmail.com>
2025-10-17 09:54:05 -0500

    Added function to search posts with query from the last year, sorted by most comments. Check each posts date, count the number of comments in the post and add that number into months dict.

--- utils/reddit_api.py
+++ utils/reddit_api.py
@@ -1,5 +1,5 @@
 import praw
-from datetime import datetime
+from datetime import datetime, timezone
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
 from transformers import pipeline
@@ -9,6 +9,62 @@ reddit = praw.Reddit(
     client_secret="hSKd1-EVEe90CjagR3RLByBt8NkFxQ",
     user_agent="sentiment_analysis_app/1.0 by YourUsername"
 )
+
+
+def show_posts():
+
+    comments = 0
+
+    for submission in reddit.subreddit("all").search("PFAS", sort="top", time_filter="year", limit = 100):
+
+        comments += submission.num_comments
+
+        print("Post title:", submission.title)
+        print(f"URL: https://reddit.com{submission.permalink}")
+        print("Comments:", submission.num_comments)
+        print(f"Date:{datetime.fromtimestamp(submission.created_utc, tz=timezone.utc).isoformat()}")
+
+    print(f"Total Comments: {comments}")
+
+
+def last_12_month():
+    '''
+    Returns a list of the last 12 months
+    '''
+    now = datetime.now(timezone.utc)
+    keys = []
+    year, month = now.year, now.month
+    month-=1
+    if month==0:
+        month=12
+        year-=1
+        
+    for _ in range(12):
+        keys.append(f"{year:04d}-{month:02d}")
+        month -= 1
+        if month == 0:
+            month = 12
+            year -= 1
+
+    return list(reversed(keys))
+
+def monthly_comment_totals(query):
+
+    month_keys = last_12_month()
+    totals = {k: 0 for k in month_keys}
+
+    for submission in reddit.subreddit("all").search(query, sort="comments", time_filter="year", limit=1000):
+
+        if submission.num_comments == 0:
+            break
+
+        dt = datetime.fromtimestamp(submission.created_utc, tz=timezone.utc)
+        key = dt.strftime("%Y-%m")
+        if key in totals:
+            totals[key] += submission.num_comments  
+
+    return totals
+
 
 def vader():
     #https://github.com/cjhutto/vaderSentiment?tab=readme-ov-file
@@ -62,7 +118,7 @@ def hf():
     # analyzer = pipeline("sentiment-analysis")
     analyzer = pipeline(model = "cardiffnlp/twitter-roberta-base-sentiment-latest")
 
-    for submission in reddit.subreddit("all").search("eurofins", limit=100):
+    for submission in reddit.subreddit("all").search("eurofins", limit=1000):
         
         submission.comments.replace_more(limit=None, threshold=1)
 
@@ -83,5 +139,7 @@ def hf():
 
 if __name__ == "__main__":
     # vader()
-    blob()
+    # blob()
     # hf()
+    months = monthly_comment_totals("PFAS")
+    print(months)
